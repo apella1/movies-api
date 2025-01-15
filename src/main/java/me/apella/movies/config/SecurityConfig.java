0688b55bfef0b75812c1e65c5a8aa4255c71f1b2
apella1 <johncollons5@gmail.com>
2025-01-15 16:23:40 +0300

    feat: add spring security and spring rest docs

---
+++ src/main/java/me/apella/movies/config/SecurityConfig.java
@@ -0,0 +1,9 @@
+package me.apella.movies.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+}
